b477ec7c5046bbc4d9bb825e02c2a1a2eddae07c
katochsenthal <katoch.senthal@gmail.com>
2022-03-25 22:22:46 -0400

    displaying day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("Ola");
+var currentDayEl = document.querySelector("#currentDay");
+var scheduleContainerEl = document.querySelector(".container");
+
+// displaying the day on top of the calender
+
+var displayDay = function () {
+  var currentDay = moment().format("MMMM Do  YYYY, h:mm:ss a");
+  currentDayEl.innerHTML = currentDay;
+};
+displayDay();
