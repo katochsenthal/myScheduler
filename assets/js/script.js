68c41cc980e8ade3b444cea5102735255aaa95ae
katochsenthal <katoch.senthal@gmail.com>
2022-03-26 14:18:34 -0400

    added save icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var scheduleContainerEl = document.querySelector(".container");
 
 var descriptionEl = document.querySelectorAll(".description");
 
+var saveBtnEl = document.querySelectorAll(".saveBtn");
+
 var currentTime = Number(moment().format("H"));
 
 var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
@@ -28,20 +30,29 @@ var checkTime = function () {
 };
 checkTime();
 
-//
-/*
-if(time === past){
-descriptionEl.style.background.color = "grey"
-#option2
-descriptionEl.addClass("past")
-}
-if(time === current){
-  descriptionEl.style.background.color = "red"
-  #option2
-descriptionEl.addClass("present")
-}
-if(time === future)
-descriptionEl.style.background.color = "green"
-#option2
-descriptionEl.addClass("future")
-*/
+//local storage
+
+var saveDescription = function (event) {
+  var time = event.target.parentNode.children[0].textContent;
+  var description = event.target.parentNode.children[1].value;
+
+  localStorage.setItem(time, description);
+};
+
+var getSchedule = function () {
+  for (var i = 0; i < descriptionEl.length; i++) {
+    var time = descriptionEl[i].parentNode.children[0].textContent;
+
+    var saveValue = localStorage.getItem(time);
+    descriptionEl[i].textContent = saveValue;
+  }
+};
+getSchedule();
+
+//event
+var addEvents = function () {
+  for (var i = 0; i < saveBtnEl.length; i++) {
+    saveBtnEl[i].addEventListener("click", saveDescription);
+  }
+};
+addEvents();
